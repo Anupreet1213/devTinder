fe48b98fba3a73cd03ff3ab2ca83877dd7da91f3
Anupreet1213 <anupreet.2003@gmail.com>
2025-01-28 15:10:01 +0530

    exploring api routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Anupreet", lastName: "Srivastava" });
+});
+
+app.post("/user", (req, res) => {
+  // Saving to DB
+  res.send("Data saved successfully!!");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello World!!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.....");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000.....");
 });
